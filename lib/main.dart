5192bd9fce45e8bd97fbfeb8bf835feb6570a608
mohamedaminehnioua <mohamedaminehnioua@gmail.com>
2024-01-24 12:07:40 +0100

    move: firebase_options

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:cookeat/core/data/local/shred_pref.dart';
-import 'package:cookeat/firebase_options.dart';
+import 'package:cookeat/core/services/firebase/firebase_options.dart';
 import 'package:cookeat/modules/app/view/app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
