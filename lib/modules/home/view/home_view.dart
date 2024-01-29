3befa454c2ad3284983cb5e30930989dbf6dbb70
mohamedaminehnioua <mohamedaminehnioua@gmail.com>
2024-01-29 15:58:14 +0100

    add: Sign out

--- lib/modules/home/view/home_view.dart
+++ lib/modules/home/view/home_view.dart
@@ -1,3 +1,8 @@
+import 'dart:async';
+
+import 'package:cookeat/core/components/components.dart';
+import 'package:cookeat/core/router/router.dart';
+import 'package:cookeat/core/services/firebase/auth/auth_services.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
@@ -5,9 +10,28 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('Home'),
+        child: ElevatedButton(
+          onPressed: () async {
+            await CustomLoadingOverlay.show(
+              asyncFunction: () async {
+                final currentContext = context;
+                final signedOut = await AuthServices.signOut();
+                if (signedOut) {
+                  unawaited(
+                    // ignore: use_build_context_synchronously
+                    Navigator.of(currentContext).pushNamed(Routes.auth),
+                  );
+                }
+              },
+            );
+          },
+          child: const CustomText(
+            'Sign out',
+            textColor: Colors.white,
+          ),
+        ),
       ),
     );
   }
